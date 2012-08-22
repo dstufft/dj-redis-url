6e435773d63d1df673d7db5acabca72adad6df98
Donald Stufft <donald.stufft@gmail.com>
2012-08-21 22:12:16 -0400

    move long_description into __doc__

--- setup.py
+++ setup.py
@@ -1,4 +1,31 @@
 #!/usr/bin/env python
+"""
+DJ-Database-URL
+===============
+
+This simple Django utility allows you to utilize the
+`12factor <http://www.12factor.net/backing-services>`_ inspired
+``REDIS_URL`` environment variable to configure your Django application.
+
+
+Usage
+-----
+
+Configure your Redis database in ``settings.py`` from ``REDIS_URL``::
+
+    REDIS = {"default": dj_redis_url.config()}
+
+Parse an arbitrary Database URL::
+
+    REDIS = {"default": dj_redis_url.parse("redis://..."")}
+
+Installation
+------------
+
+Installation is simple too::
+
+    $ pip install dj-redis-url
+"""
 from setuptools import setup, find_packages
 
 setup(
@@ -6,9 +33,8 @@ setup(
     version="0.1.1",
 
     description="Use Redis URLs in your Django Application.",
-    long_description=open("README.rst").read(),
+    long_description=__doc__,
     url="https://github.com/dstufft/dj-redis-url",
-    license=open("LICENSE").read(),
 
     author="Donald Stufft",
     author_email="donald.stufft@gmail.com",
