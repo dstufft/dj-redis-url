dd37a55a77f7c9ce5423fd5a0511e0f32d4ca091
Donald Stufft <donald.stufft@gmail.com>
2012-08-21 22:13:41 -0400

    bump version

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="dj-redis-url",
-    version="0.1.1",
+    version="0.1.2",
 
     description="Use Redis URLs in your Django Application.",
     long_description=__doc__,
